c98f27aceef31196f876e7554d563c9710dd72e1
mohammad <mohammadhoseinajorloo76@gmail.com>
2026-08-20 19:09:53 +0330

    feat(zsh): Add new rprompt for lab enviroment in zsh

--- common/.config/zsh/init.zsh
+++ common/.config/zsh/init.zsh
@@ -5,11 +5,16 @@ precmd() {
   RPROMPT_PARTS=()
 
   if [[ -n "$vcs_info_msg_0_" ]]; then
-    RPROMPT_PARTS+=("%F{238}%f%F{0}%K{238}  ${vcs_info_msg_0_} ")
+    RPROMPT_PARTS+=("%F{238}%f%F{0}%K{238}  ${vcs_info_msg_0_} %f")
   fi
 
   if [[ -n "$VENV_PROMPT" ]]; then
-    RPROMPT_PARTS+=("%F{245}%f%F{0}%K{245}  ${VENV_PROMPT} %f ")
+    RPROMPT_PARTS+=("%F{245}%f%F{0}%K{245}  ${VENV_PROMPT} %f")
+  fi
+
+  # Show an indicator only inside a disposable `lab` session.
+  if [[ "${LAB_ACTIVE:-}" == "1" ]]; then
+    RPROMPT_PARTS+=("%F{245}%f%F{0}%K{245} ⚗️ ${LAB_TOPIC} %f")
   fi
 
   RPROMPT="${(j: :)RPROMPT_PARTS}"
