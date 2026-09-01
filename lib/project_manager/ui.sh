eb96b67a7f98cd8a01ef0f9e167c96c09a10ee24
mohammad <mohammadhoseinajorloo76@gmail.com>
2026-09-01 15:04:50 +0330

    bug(tpm): Fixed fzf preview in tpm (project manger)
    - Delete previw part from ui file in lib folde
    - Add `tmux-session-preview` script for rendering preview in fzf
    - Update `project-manger` script for read preview script
    
    Ref: #7

--- lib/project_manager/ui.sh
+++ lib/project_manager/ui.sh
@@ -1,35 +1,9 @@
 #!/usr/bin/env bash
 
-generate_preview_cmd() {
-  cat << 'EOF'
-session="{}"
-printf "\033[1;36mSession:\033[0m %s\n\n" "$session"
-
-tmux display-message \
-  -p \
-  -t "${session}:0.0" \
-  "Path: #{pane_current_path}" 2>/dev/null
-
-printf "\n\033[1;36mWindows:\033[0m\n"
-tmux list-windows \
-  -t "$session" \
-  -F " #{window-index}: #{window_name}" 2>/dev/null
-
-printf "\n\033[1;36mPane output:\033[0m\n"
-tmux capture-pane \
-  -p \
-  -e \
-  -t "${session}:0.0" \
-  -S -80 2>/dev/null
-
-EOF
-}
-
 select_project_fzf() {
   local current_session="$1"
   local list_helper="$2"
-  local preview_cmd
-  preview_cmd="$(generate_preview_cmd)"
+  local preview_script="$3"
 
   "$list_helper" "$current_session" |
     fzf \
@@ -39,8 +13,8 @@ select_project_fzf() {
     --height=100% \
     --layout=reverse \
     --border \
-    --preview="$preview_cmd" \
-    --preview-window='right:40%:wrap' \
+    --preview="$preview_script {}" \
+    --preview-window='right:65%:wrap' \
     --bind="ctrl-d:execute-silent(tmux kill-session -t '{}')+reload($list_helper '$current_session')" \
     --header='CTRL-D: Delete | ENTER: Attach/Switch'
 }
