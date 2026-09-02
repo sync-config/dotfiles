281398f987fcc87f4e2c5b204d9b46cf1cfcfcd3
mohammad <mohammadhoseinajorloo76@gmail.com>
2026-09-02 22:06:33 +0330

    refacto(awesome): resolve audio script path dynamically via gears.filesystem
    Replace hardcoded $HOME path concatenation with
    gears.filesystem.get_configuration_dir() to ensure robst and dynamic
    config directory resolutio.
    
    Refs: #16

--- common/.config/awesome/config/audio.lua
+++ common/.config/awesome/config/audio.lua
@@ -3,8 +3,7 @@ local gears = require("gears")
 
 local M = {}
 
-local home = os.getenv("HOME")
-local audio_script = home .. "/.config/awesome/scripts/audio.sh"
+local audio_script = gears.filesystem.get_configuration_dir() .. "scripts/audio.sh"
 
 function M.auto()
   awful.spawn.with_shell(audio_script .. " auto >/dev/null 2>&1")
